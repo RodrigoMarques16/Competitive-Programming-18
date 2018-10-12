ad528ab37bca11ddaa19348ba0f552481bac90a2
Rodrigo Marques <rodrigocanedomarques@hotmail.com>
2018-10-12 08:09:41 +0100

    Update 11136.cpp

--- uva/11136.cpp
+++ uva/11136.cpp
@@ -8,24 +8,24 @@ using namespace std;
 // 1136 - Hoax or what
 // For every sale, its bill can be inserted in a
 // ballot. Each day, the difference between the
-// highest and lowest bills in the ballot 
+// highest and lowest bills in the ballot are
 // awarded to a customer. 
 // 
 // The ballot is not emptied at the end of the 
-// day
+// day.
 //
 // Given n days, calculate the total cost of 
-// the promotion
+// the promotion.
 
 // This problem is pretty straight forward.
 //
-// To simulate the ballot we can use a multiset,
-// the lowest bill will be at the beginning and
-// the highest at the end.
+// With a multiset, we can insert bills into
+// it in every repetition while it stays sorted.
+// The lowest bill will be at the beggining, the
+// highest bill at end.
 //
-// Then, at the end of each day simply remove
-// those bills from the set, calculate the 
-// diference, and increase the total cost.
+// Knowing that we can just remove those bills
+// from the set and calculate the cost for that day
  
 int main() {
 
