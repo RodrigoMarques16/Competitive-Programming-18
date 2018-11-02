6700e472008cf0e6c2186e3d419c2a9e88f04d2f
Rodrigo Marques <rodrigocanedomarques@hotmail.com>
2018-11-02 09:22:59 +0000

    racetime dumb

--- spoj/racetime.cpp
+++ spoj/racetime.cpp
@@ -5,12 +5,13 @@
 
 #define DEBUG(x) cout << #x << ": " << x << endl;
 #define N 100000
+#define M 1000000005
 
 using namespace std;
 
 // SPOJ - RACETIME
-//
-//
+// disguise naive as sqrt decomposition and it's actually 
+// 0.06 seconds faster
 
 int n;
 int bucket_size;
@@ -29,12 +30,6 @@ int count(int i, int j, int val) {
 }
 
 int query(int x, int y, int val) {
-
-    if (val == 0)  return 0;
-    if (x == y)
-        if (val <= v[x]) return 1;
-        else return 0;
-
     int k = 0;
     int a = bucket[x];
     int b = bucket[y];
@@ -43,6 +38,7 @@ int query(int x, int y, int val) {
     if (a == b) { 
         return count(x, y, val);
     }
+
     // first bucket
     for (int i = x; i <= end_index[a] && i <= y; i++) {
         if (v[i] <= val)
@@ -57,21 +53,21 @@ int query(int x, int y, int val) {
 
     // everything in between
     for(int i = a+1; i < b; i++) {
-        if (max_in_bucket[i] <= val) {
+        if (max_in_bucket[i] <= val)
             k += bucket_size;
-        }
         else k += count(start_index[i], end_index[i], val);
     }
 
     return k;
 }
 
-int update(int i, int val) {
+void update(int i, int val) {
     v[i] = val;
     int b = bucket[i];
-    max_in_bucket[b] = -1;
-    for(int i = start_index[b]; i <= end_index[b]; i++)
+    max_in_bucket[b] = val;
+    for(int i = start_index[b]; i <= end_index[b]; i++) {
         max_in_bucket[b] = std::max(max_in_bucket[b], v[i]);
+    }
 }
 
 int main() {
@@ -85,11 +81,12 @@ int main() {
 
     int b = 0;
     start_index[1] = 0;
-
+    max_in_bucket[0] = -1;
     for(int i = 0; i < n; i++) {
         if (i % bucket_size == 0) {
             end_index[b] = i-1;
             b++;
+            max_in_bucket[b] = -1;
             start_index[b] = i;
         }
         cin >> v[i];
